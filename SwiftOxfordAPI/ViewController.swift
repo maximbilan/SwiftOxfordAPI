86333eead79b5fab5a64d40e2584f7e7fbcc08b7
Maxim Bilan <maximb.mail@gmail.com>
2018-11-11 13:15:35 +0200

    Added a project template

---
+++ SwiftOxfordAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftOxfordAPI
+//
+//  Created by Maxim on 11/11/18.
+//  Copyright © 2018 maximbilan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
